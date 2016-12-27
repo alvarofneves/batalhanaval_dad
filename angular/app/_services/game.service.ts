208eb642a825e422389b3c91f68b4b7cfc6012a6
a-santos <2130653@my.ipleiria.pt>
2016-12-27 22:59:59 +0000

    Editei construtor 'game'

--- angular/app/_services/game.service.ts
+++ angular/app/_services/game.service.ts
@@ -1,6 +1,17 @@
 import { Injectable }                              from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
-import { Player }     from '../_shared/index';
+import { Game }     from '../_shared/index';
 import { Observable } from 'rxjs/Rx';
 
+@Injectable()
+export class PlayerService {
+    constructor(private http: Http) { }
+
+    //create(game: Game): Observable<any> {
+    //}
+
+    getAll() {
+
+    }
+}
