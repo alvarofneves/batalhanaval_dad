854b6ee2e6e38bf483d7b68a018ed4f1d275dd9f
a-santos <2130653@my.ipleiria.pt>
2017-01-03 11:59:22 +0000

    Notifics OK no lobby. Irei mudar service para pasta dos outros

--- angular/app/_services/index.ts
+++ angular/app/_services/index.ts
@@ -1,4 +1,5 @@
 export * from './alert.service';
+export * from './game.service';
 export * from './authentication.service';
 export * from './player.service';
-export * from './game.service';
+export * from './websocket.service';
--- angular/app/lobby/lobby.module.ts
+++ angular/app/lobby/lobby.module.ts
@@ -1,14 +1,20 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { LobbyComponent }   from './lobby.component';
+import { LobbyComponent }   	from './lobby.component';
+import { ChatComponent } 		from '../chat/chat.component';
+import { NotificationsModule }  from '../notifications/notifications.module';
 
 @NgModule({
 	imports: [ 
 		BrowserModule, 
+		FormsModule,
+		NotificationsModule
 	],
 	declarations: [ 
-		LobbyComponent
+		LobbyComponent,
+		ChatComponent
 	],
 	exports: [ 
 		LobbyComponent 
